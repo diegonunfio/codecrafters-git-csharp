97caaabd340388454e25c125ef7860d5e0b65e64
Diego Nunfio <dg.nunfio@gmail.com>
2025-05-10 22:59:37 -0600

    third commit

--- src/Program.cs
+++ src/Program.cs
@@ -4,24 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length == 1 && args[0] == "init")
+        try
         {
-            GitCommands.Init();
+            if (args.Length == 1 && args[0] == "init")
+            {
+                GitCommands.Init();
+            }
+            else if (args.Length == 3 && args[0] == "cat-file" && args[1] == "-p")
+            {
+                GitCommands.CatFile(args[2]);
+            }
+            else if (args.Length == 3 && args[0] == "hash-object" && args[1] == "-w")
+            {
+                GitCommands.HashObject(args[2]);
+            }
+            else
+            {
+                Console.Error.WriteLine("Comando no reconocido.");
+                Environment.Exit(1);
+            }
         }
-        
-        else if (args.Length == 3 && args[0] == "cat-file" && args[1] == "-p")
+        catch (Exception ex)
         {
-            GitCommands.CatFile(args[2]);
-        }
-        else if (args.Length == 3 && args[0] == "hash-object" && args[1] == "-w")
-        {
-            GitCommands.HashObject(args[2]);
-        }
-        else
-        {
-            Console.Error.WriteLine("Comando no reconocido.");
+            Console.Error.WriteLine("Excepción no controlada: " + ex.Message);
             Environment.Exit(1);
         }
-        
     }
 }
