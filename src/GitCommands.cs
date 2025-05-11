c2bb65fc75bc3b63ec0fccb11531635ed1bd7896
Diego Nunfio <dg.nunfio@gmail.com>
2025-05-10 22:26:46 -0600

    second commit

---
+++ src/GitCommands.cs
@@ -0,0 +1,38 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace codecrafters_git;
+
+public class GitCommands
+{
+    public static void CatFile(string hash)
+    {
+        string gitObjectsPath = Path.Combine(".git", "objects", hash.Substring(0, 2), hash.Substring(2));
+        
+        if (!File.Exists(gitObjectsPath))
+        {
+            Console.Error.WriteLine("Error: objeto no encontrado");
+            Environment.Exit(1);
+        }
+
+        byte[] compressed = File.ReadAllBytes(gitObjectsPath);
+
+        using (var compressedStream = new MemoryStream(compressed))
+        using (var deflateStream = new DeflateStream(compressedStream, CompressionMode.Decompress))
+        using (var resultStream = new MemoryStream())
+        {
+            deflateStream.CopyTo(resultStream);
+            byte[] decompressed = resultStream.ToArray();
+
+            // Buscar el byte 0 que separa el header del contenido
+            int nullIndex = Array.IndexOf(decompressed, (byte)0);
+
+            // Extraer solo el contenido del blob (después del byte nulo)
+            byte[] contentBytes = new byte[decompressed.Length - nullIndex - 1];
+            Array.Copy(decompressed, nullIndex + 1, contentBytes, 0, contentBytes.Length);
+
+            // Imprimir sin salto de línea final
+            Console.Write(Encoding.UTF8.GetString(contentBytes));
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,28 +1,20 @@
 using System;
 using System.IO;
+using System.IO.Compression;
+using System.Text;
+using codecrafters_git;
 
-if (args.Length < 1)
+class Program
 {
-    Console.WriteLine("Please provide a command.");
-    return;
-}
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.Error.WriteLine("Logs from your program will appear here!");
-
-string command = args[0];
-
-if (command == "init")
-{
-    // Uncomment this block to pass the first stage
-    
-    Directory.CreateDirectory(".git");
-    Directory.CreateDirectory(".git/objects");
-    Directory.CreateDirectory(".git/refs");
-    File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
-     Console.WriteLine("Initialized git directory");
-}
-else
-{
-    throw new ArgumentException($"Unknown command {command}");
+    static void Main(string[] args)
+    {
+        if (args.Length == 3 && args[0] == "cat-file" && args[1] == "-p")
+        {
+            GitCommands.CatFile(args[2]);
+        }
+        else
+        {
+            Console.Error.WriteLine("Comando no reconocido.");
+        }
+    }
 }
